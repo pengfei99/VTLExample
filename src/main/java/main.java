a8db070bb3e5df43f9296f3faa92ba9762ddd015
pengfei <liu.pengfei@hotmail.fr>
2022-02-09 11:57:25 +0100

    add vtl example

---
+++ src/main/java/main.java
@@ -0,0 +1,61 @@
+import fr.insee.vtl.model.Dataset;
+import fr.insee.vtl.model.InMemoryDataset;
+
+import javax.script.*;
+import java.util.List;
+import java.util.Map;
+
+public class main {
+
+    public static void main(String[] args) throws ScriptException {
+
+        // create a script engine manager
+        ScriptEngineManager engineFac = new ScriptEngineManager();
+        // create a vtl engine
+        ScriptEngine engine = engineFac.getEngineByName("vtl");
+
+
+         // create a dataset
+        // By default, if a variable role is not defined, the `MEASURE` will be affected.
+        InMemoryDataset dataset = new InMemoryDataset(
+                List.of(
+                        Map.of("name", "Alice", "sex", "F", "age", 25),
+                        Map.of("name", "Bob", "sex", "M", "age", 30)
+                ),
+                Map.of("name", String.class, "sex", String.class, "age", Integer.class),
+                Map.of("name", Dataset.Role.IDENTIFIER,"sex", Dataset.Role.MEASURE,"age", Dataset.Role.MEASURE)
+
+        );
+
+        // load dataset into binding
+        Bindings bindings = new SimpleBindings();
+        bindings.put("users", dataset);
+
+        // add bindings(dataset) to engine context
+        ScriptContext context = engine.getContext();
+        context.setBindings(bindings, ScriptContext.ENGINE_SCOPE);
+
+        // define a validation rule on dataset, column age can't have null values
+        String transformation= "res := users[filter age > 6];";
+        String validation_rule = "res := check(users[filter not isnull(age)] >= 1 " +
+                "errorcode \"Values, when provided, should be higher or equal to 1\"\n" +
+                "errorlevel \"Warning\");";
+
+        // apply validation rule a
+        try {
+            engine.eval(transformation);
+        } catch (ScriptException e) {
+            e.printStackTrace();
+        }
+
+        Bindings outputBindings = engine.getContext().getBindings(ScriptContext.ENGINE_SCOPE);
+
+        InMemoryDataset res = (InMemoryDataset) outputBindings.get("res");
+
+        List<List<Object>> resList = res.getDataAsList();
+        System.out.println(resList);
+
+
+    }
+
+}
